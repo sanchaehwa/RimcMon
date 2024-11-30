d10963600a87d7d587cd79b2c73882570bc55351
krdevdory <krdevmon@gmail.com>
2024-11-30 21:57:19 +0900

    #9 [Feat] Python - 크롤링 데이터 데이터베이스 추가 기능 개발

--- Python_Script/insert_to_db.py
+++ Python_Script/insert_to_db.py
@@ -1,19 +1,26 @@
 import requests
+import sys
+import os
+# 현재 파일의 상위 디렉토리를 경로에 추가
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+from Crawler import get_contest_data
 
 # 요청을 보낼 URL
 url = "http://opsw4.dothome.co.kr/insert_into_crawl_data.php"
-params = { # GET 요청의 쿼리 파라미터
-    # 데이터 포맷
-    "title": "제목 데이터",
-    "date": "내용 ~~~",
-    "image": "https://img2.stibee.com/36150_2328124_1723012489158038678.PNG",    
-}
 
-# GET 요청 보내기
-response = requests.get(url, params=params)
+for i in get_contest_data():
+    params = { # GET 요청의 쿼리 파라미터
+        # 데이터 포맷
+        "title": i['title'],
+        "date": i['date'],
+        "image": i['image'],
+    }
 
-# 응답 출력
-if response.status_code == 200:
-    print("응답 성공:", response.text)
-else:
-    print("요청 실패:", response.status_code)
+    # GET 요청 보내기
+    response = requests.get(url, params=params)
+
+    # 응답 출력
+    if response.status_code == 200:
+        print("응답 성공:", response.text)
+    else:
+        print("요청 실패:", response.status_code)
