ae21768b94705ea25887b0e6b877860bd13ed5cc
bum0w0 <kgy5988@naver.com>
2024-12-05 14:01:06 +0900

    #18 [Feat] 사용자 입력 데이터 정리 및 서버 전송 기능 추가

--- Python_Script_Profile/insert_to_db.py
+++ Python_Script_Profile/insert_to_db.py
@@ -1,32 +1,32 @@
+import logging
 import requests
-import sys
-import os
-# 현재 파일의 상위 디렉토리를 경로에 추가
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-# from Profile_FE.profile import get_user_data
 
-def get_user_data():
-    return [
-        {"name": "John Doe", "bio": "Developer", "tool": "https://img.shields.io/badge/Adobe Photoshop-31A8FF?style=flat-square&logo=Adobe Photoshop&logoColor=white"},
-        {"name": "Jane Smith", "bio": "Designer", "tool": "https://img.shields.io/badge/Adobe Illustrator-FF9A00?style=flat-square&logo=Adobe Illustrator&logoColor=white"}
-    ]
+# 로깅 설정
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 # 요청을 보낼 URL
 url = "http://opsw4.dothome.co.kr/insert_into_user_data.php"
 
-for i in get_user_data():
-    params = { # GET 요청의 쿼리 파라미터
-        # 데이터 포맷
-        "name": i['name'],
-        "bio": i['bio'],
-        "tool": i['tool'],
+# 데이터 전송 함수
+def send_data_to_server(data):
+    params = {
+        "name": data['name'],
+        "bio": data['bio'],
+        "tool": data['tools'],
     }
 
-    # GET 요청 보내기
     response = requests.get(url, params=params)
 
-    # 응답 출력
     if response.status_code == 200:
         print("응답 성공:", response.text)
     else:
         print("요청 실패:", response.status_code)
+
+# 데이터 합치기 (JSON 반환)
+def combine_data_for_db(skills, session_state):
+    return {
+        "name": session_state.name.strip(),
+        "bio": session_state.self_intro.strip(),
+        "tools": [skills[skill] for skill in session_state.selected_skills],
+    }
+
