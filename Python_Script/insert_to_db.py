50aea633f2cda2c26e346d03a99875d91a48f66a
krdevdory <krdevmon@gmail.com>
2024-12-16 13:11:23 +0900

    #45[Feat] 관리자 페이지에서 크롤링 및 데이터 삭제 기능 추가

--- Python_Script/insert_to_db.py
+++ Python_Script/insert_to_db.py
@@ -8,33 +8,35 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 from Crawling.Crawler import get_contest_data
 from Crawling.LinkCrawler import get_link_data
 
-# 요청을 보낼 URL
-url = "http://opsw4.dothome.co.kr/insert_into_crawl_data.php"
 
-# 공모전 데이터 가져오기
-contest_data = get_contest_data()
+def insert_mysql_data():
+    # 요청을 보낼 URL
+    url = "http://opsw4.dothome.co.kr/insert_into_crawl_data.php"
 
-# 모집 페이지 데이터 가져오기
-link_data = json.loads(get_link_data())  # JSON 문자열을 Python 객체로 변환
+    # 공모전 데이터 가져오기
+    contest_data = get_contest_data()
 
-# 모집 페이지 데이터를 `contest_data`에 병합
-for idx, contest in enumerate(contest_data):
-    # 모집 페이지가 존재하면 병합, 없으면 기본값 설정
-    contest["page"] = link_data[idx]["recruitment_page"] if idx < len(link_data) else "N/A"
+    # 모집 페이지 데이터 가져오기
+    link_data = json.loads(get_link_data())  # JSON 문자열을 Python 객체로 변환
 
-    # GET 요청의 쿼리 파라미터
-    params = {
-        "title": contest['title'],
-        "date": contest['date'],
-        "image": contest['image'],
-        "page": contest['page'],  # 추가된 page 데이터
-    }
+    # 모집 페이지 데이터를 `contest_data`에 병합
+    for idx, contest in enumerate(contest_data):
+        # 모집 페이지가 존재하면 병합, 없으면 기본값 설정
+        contest["page"] = link_data[idx]["recruitment_page"] if idx < len(link_data) else "N/A"
 
-    # GET 요청 보내기
-    response = requests.get(url, params=params)
+        # GET 요청의 쿼리 파라미터
+        params = {
+            "title": contest['title'],
+            "date": contest['date'],
+            "image": contest['image'],
+            "page": contest['page'],  # 추가된 page 데이터
+        }
 
-    # 응답 출력
-    if response.status_code == 200:
-        print("응답 성공:", response.text)
-    else:
-        print("요청 실패:", response.status_code)
+        # GET 요청 보내기
+        response = requests.get(url, params=params)
+
+        # 응답 출력
+        if response.status_code == 200:
+            print("응답 성공:", response.text)
+        else:
+            print("요청 실패:", response.status_code)
--- Python_Script/reset_data.py
+++ Python_Script/reset_data.py
@@ -1,14 +1,15 @@
 import requests
 
-# 요청을 보낼 URL
-url = "http://opsw4.dothome.co.kr/truncate_crawl_data.php"
+def reset_mysql_data():
+    # 요청을 보낼 URL
+    url = "http://opsw4.dothome.co.kr/truncate_crawl_data.php"
 
-try:
-    response = requests.get(url)    # GET 요청 보내기
-    if response.status_code == 200: # 응답 상태 확인
-        print("PHP 응답:")
-        print(response.text)      # 응답 확인
-    else:
-        print(f"PHP 응답 오류: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print(f"HTTP 요청 중 오류 발생: {e}")
+    try:
+        response = requests.get(url)    # GET 요청 보내기
+        if response.status_code == 200: # 응답 상태 확인
+            print("PHP 응답:")
+            print(response.text)      # 응답 확인
+        else:
+            print(f"PHP 응답 오류: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"HTTP 요청 중 오류 발생: {e}")
